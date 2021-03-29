1b55857ef3b1aa878cf7711217e8563b0d223607
Keith Beers <theeskribe@gmail.com>
2021-03-29 19:23:16 +0000

    added Cendor info

--- lab_input/iptaker02.py
+++ lab_input/iptaker02.py
@@ -5,3 +5,6 @@ user_input = input("Please enter your IPv4 IP address:")
 # print("You told me the IPv4 address is:" + user_input)
 
 print("You told me the IPv4 address is:", user_input)
+
+vendor_name = input("Please enter your vendor name:")
+print("You told me the vnedor name is:", vendor_name)
